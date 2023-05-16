b904bf6f7602501cbcf3cccb3a4fef00d11d6a6c
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2023-05-16 19:58:05 +0300

    education ground updated

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -5,16 +5,16 @@ export default function Contact() {
     <section id="contact" className="Contact">
       <div className="container">
         <h2 className="section-title" style={{ textAlign: "center" }}>Contact</h2>
-        <div className="row" style={{ display: "flex", justifyContent: "center"}}>
-          <div className="col-3 info-card">
-              <HomeFilled style={{ fontSize: 40, marginBottom: 3 }} />
-              <h4 style={{ textAlign: "center", fontSize: 18, marginBottom: 3}}>Izmir, Turkey</h4>
-              <p style={{ textAlign: "center", marginBottom: 0}}>Ödemis, TR</p>
+        <div className="row" style={{ display: "flex", justifyContent: "center" }}>
+          <div className="col-md-3 col-sm-12 info-card">
+            <HomeFilled style={{ fontSize: 40, marginBottom: 3 }} />
+            <h4 style={{ textAlign: "center", fontSize: 18, marginBottom: 3 }}>Izmir, Turkey</h4>
+            <p style={{ textAlign: "center", marginBottom: 0 }}>Ödemis, TR</p>
           </div>
-          <div className="col-3 info-card">
-              <MailFilled style={{ fontSize: 40, marginBottom: 3 }} />
-              <h4 style={{ textAlign: "center", fontSize: 18, marginBottom: 3}}>Mail me from:</h4>
-              <a rel="noreferrer" className="mail-link" style={{ textAlign: "center", marginBottom: 0}}>m.kemalgordesli@gmail.com</a>
+          <div className="col-md-3 col-sm-12 info-card">
+            <MailFilled style={{ fontSize: 40, marginBottom: 3 }} />
+            <h4 style={{ textAlign: "center", fontSize: 18, marginBottom: 3 }}>Mail me from:</h4>
+            <a href="mailto:m.kemalgordesli@gmail.com" rel="noreferrer" className="mail-link" style={{ textAlign: "center", marginBottom: 0 }}>m.kemalgordesli@gmail.com</a>
           </div>
         </div>
       </div>
--- src/views/Resume/Resume.jsx
+++ src/views/Resume/Resume.jsx
@@ -37,85 +37,108 @@ export default function Projects() {
               </a>
             </span>
             <span className="btn-span">
-              <a className="btn btn-text" rel="noreferrer">SKILLS</a>
+              <a className="btn btn-text" rel="noreferrer">
+                SKILLS
+              </a>
             </span>
           </div>
         </div>
       </div>
 
-      <div className="container mt-4" id="education">
-        <div className="row">
-          <div className="col col-lg-6 col-md-4 col-sm-4 col-xs-2">
-            <h3 className="title-text">EDUCATION</h3>
-            <div style={{ marginBottom: 10, paddingTop: 10 }}>
-              <p className="content-text">
-                Bachelor of Science in Computer Engineering
-              </p>
-              <p className="content-text">
-                <a
-                  rel="noreferrer"
-                  href="https://www.balikesir.edu.tr"
-                  target="_blank"
-                  className="edu-link"
-                >
-                  Balıkesir University
-                </a>
-                , 2020 -{" "}
-              </p>
-            </div>
-            <div style={{ marginBottom: 10 }}>
-              <p className="content-text">High School</p>
-              <p className="content-text">
-                <a
-                  rel="noreferrer"
-                  href="https://kirazal.meb.k12.tr/"
-                  target="_blank"
-                  className="edu-link"
-                >
-                  Kiraz Anatolian High School
-                </a>
-                , 2016 - 2020
-              </p>
-            </div>
-          </div>
-          <div className="col col-lg-6 col-md-8 col-sm-8 col-xs-10"></div>
-        </div>
-      </div>
-
-      <div className="container mt-4" id="experience">
-        <div className="row">
-          <div className="col col-lg-6 col-md-4 col-sm-4 col-xs-2">
-            <h3 className="title-text">EXPERIENCE</h3>
-            <div style={{ marginBottom: 10, paddingTop: 10 }}>
-              <p className="content-title-text">
-                <a
-                  rel="noreferrer"
-                  href="https://www.loscart.com/"
-                  target="_blank"
-                  className="company-link"
-                >
-                  Loscart
-                </a>{" "}
-                - Backend Developer Intern
-              </p>
-              <p className="content-text">September 2022 - February 2023</p>
-            </div>
-            <div style={{ marginBottom: 10, paddingTop: 10 }}>
-              <p className="content-title-text">
-                <a
-                  rel="noreferrer"
-                  href="https://www.vbt.com.tr/en"
-                  target="_blank"
-                  className="company-link"
-                >
-                  VBT Software Inc.
-                </a>{" "}
-                - Software Engineering Intern
-              </p>
-              <p className="content-text">June 2022 - August 2022</p>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-lg-2">
+          <div className="col">
+            <div
+              className="container mt-4"
+              id="education"
+              style={{
+                marginRight: "auto",
+                marginLeft: "auto",
+                maxWidth: 385
+              }}
+            >
+              <div className="row">
+                <div className="col">
+                  <h3 className="title-text">EDUCATION</h3>
+                  <div style={{ marginBottom: 10, paddingTop: 10 }}>
+                    <p className="content-text">
+                      Bachelor of Science in Computer Engineering
+                    </p>
+                    <p className="content-text">
+                      <a
+                        rel="noreferrer"
+                        href="https://www.balikesir.edu.tr"
+                        target="_blank"
+                        className="edu-link"
+                      >
+                        Balıkesir University
+                      </a>
+                      , 2020 -{" "}
+                    </p>
+                  </div>
+                  <div style={{ marginBottom: 10 }}>
+                    <p className="content-text">High School</p>
+                    <p className="content-text">
+                      <a
+                        rel="noreferrer"
+                        href="https://kirazal.meb.k12.tr/"
+                        target="_blank"
+                        className="edu-link"
+                      >
+                        Kiraz Anatolian High School
+                      </a>
+                      , 2016 - 2020
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col col-lg-6 col-md-8 col-sm-8 col-xs-10"></div>
+          <div className="col">
+            <div className="container mt-4" id="experience"  style={{
+                marginRight: "auto",
+                marginLeft: "auto",
+                maxWidth: 375
+              }}>
+              <div className="row">
+                <div className="col">
+  
+                  <h3 className="title-text">EXPERIENCE</h3>
+                  <div style={{ marginBottom: 10, paddingTop: 10 }}>
+                    <div className="content-title-text">
+                      <a
+                        rel="noreferrer"
+                        href="https://www.loscart.com/"
+                        target="_blank"
+                        className="company-link"
+                      >
+                        Loscart
+                      </a>{" "}
+                      - Backend Developer Intern
+                    </div>
+                    <p className="content-text">
+                      September 2022 - February 2023
+                    </p>
+                  </div>
+                  <div style={{ marginBottom: 10,  }}>
+                    <div className="content-title-text">
+                      <a
+                        rel="noreferrer"
+                        href="https://www.vbt.com.tr/en"
+                        target="_blank"
+                        className="company-link"
+                      >
+                        VBT Software Inc.
+                      </a>{" "}
+                      - Software Engineering Intern
+                    </div>
+                    <p className="content-text">June 2022 - August 2022</p>
+                  </div>
+      
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
